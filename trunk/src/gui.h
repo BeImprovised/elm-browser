cce0cbb21dcc81eff7f4826d6a429ccda26eafc7
cchandel@gmail.com <cchandel@gmail.com@4bee0d8c-0064-11df-b631-23daf5ca4ffa>
2010-01-17 09:09:53 +0000

    ventura commit

--- trunk/src/gui.h
+++ trunk/src/gui.h
@@ -20,8 +20,14 @@
 extern Evas_Object *view, *page, *frame, *win, *bt;
 extern Evas *e;
 extern EWebKit_Hit_Test_Contents contents;
-extern int x, y, mouse_down, old_x, old_y;
+extern int x, y, mouse_down, old_x, old_y, rotate, show_images, version, req;
+extern char start_page[255], user_agent[255], browser_dir[255], home_dir[255], req_page[255];
 
-Evas_Object *url_notify, *menu, *menu_bt, *en;
+Evas_Object *url_notify, *menu, *menu_bt, *en, *set_page_win, *rotate_ck;
+Evas_Object *show_image_ck, *user_agent_en, *start_page_en, *bookmark_win;
+Evas_Object *add_bookmark_win, *name_en, *url_en, *full_screen_ck;
+
+Elm_Genlist_Item_Class itc_gl;
 
 void create_gui(Evas_Object *win);
+void populate_bookmarks(Evas_Object *li);
